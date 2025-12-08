dd6028f54d4e47235ee778ba89d0d3a29e54d4cc
Davi Rodrigues <daviroodrigues@gmail.com>
2025-12-07 21:52:56 -0300

    feat(product-details): add buy and add to cart buttons - introduce dedicated "comprar" button for direct purchase - add "adicionar ao carrinho" button for adding product to cart - improve user flow for purchasing and cart management
    style(product-details): update product price text color
    - replace 'text-dark' with 'primary-text-color' for price display
    - align price text color with primary theme

--- views/products/details.php
+++ views/products/details.php
@@ -27,14 +27,26 @@
 
                     <div class="d-flex align-items-end mb-4 flex-column align-items-start">
                         <div class="mt-1 d-flex align-items-end flex-column">
-                            <span class="text-success small fw-bold ms-2">À vista no Pix</span>
-                            <span class="fs-1 fw-bold text-dark"><?= $product->getFormattedCashPrice() ?></span>
+                            <span class="text-success small fw-bold ms-2 ">À vista no Pix</span>
+                            <span class="fs-1 fw-bold primary-text-color"><?= $product->getFormattedCashPrice() ?></span>
                         </div>
                         <div>
                             <span class="text-muted small">Ou a prazo por apenas: </span>
                             <span class="d-inline-block text-muted fs-5">
                                 <?= $product->getFormattedInstallmentPrice() ?>
                             </span>
+                        </div>
+                        <div class="mt-auto w-100">
+
+                            <div class="d-flex gap-2 align-items-center">
+                                <a href="/produto?id=<?= $product->id ?>" class="btn btn-buy rounded-pill flex-grow-1 fw-bold">
+                                    Comprar
+                                </a>
+
+                                <a href="/carrinho/adicionar?id=<?= $product->id ?>" class="btn-add-cart shadow-sm" title="Adicionar ao Carrinho">
+                                    <i class="fa-solid fa-cart-plus"></i>
+                                </a>
+                            </div>
                         </div>
 
                         <?php if ($product->discountPercentage > 0): ?>
